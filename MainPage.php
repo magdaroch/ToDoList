4ef2bab224819fcca75a477fc8c678c48fa42dbb
MagdaRoch <magdarochminska@gmail.com>
2017-10-22 21:19:33 +0200

    adding Main Page

---
+++ MainPage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>To do List</title>
+</head>
+<body>
+
+<h1>To do list</h1>
+<form action="TaskForm.php" method="get">
+      <button type="submit">Add New Task</button>
+</form>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Magda
+ * Date: 2017-10-22
+ * Time: 21:13
+ */
+?>
+</body>
+</html>
